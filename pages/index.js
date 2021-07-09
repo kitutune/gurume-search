6326497efbd4d4602b8ecfc882a4c0f748a49a80
kitutune <p.kitutune@gmail.com>
2021-07-09 19:43:26 +0900

    🚧 nextjsのgetServerSidePropsの構文を貼る

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,24 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
+// ?format=jsonでjson形式に変更
+const defaultEndpoint = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&format=json&large_area=Z011`;
+// 下記は(https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering)の構文
+export async function getServerSideProps() {
+  const res = await fetch(`https://...`);
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
+
 export default function Home() {
-  // ?format=jsonでjson形式に変更
-  const defaultEndpoint = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&format=json&large_area=Z011`;
   return (
     <div className={styles.container}>
       <Head>
