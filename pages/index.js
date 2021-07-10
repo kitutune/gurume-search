be03b09422b6c833c04e0e938e1abd7acf6c0532
kitutune <p.kitutune@gmail.com>
2021-07-10 09:58:15 +0900

    ✨ APIから抽出した店舗データをレンダリング

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
@@ -21,6 +22,49 @@ export async function getServerSideProps() {
 
 export default function Home({ data }) {
   // コンソール画面に表示
-  console.log(data.results);
-  return <></>;
+  console.log(data.results.shop);
+  return (
+    <>
+      <Head>
+        <title>東京グルメ店検索</title>
+      </Head>
+
+      <ul>
+        {data.results.shop.map((item, index) => {
+          return (
+            <li key={index}>
+              <Link href={item.urls.pc}>
+                {/* data.result.shop.item.urls.pc */}
+                {/* [複数要素].店舗URL.PC向けURL */}
+                <a>
+                  <div>
+                    <div>
+                      <div>
+                        <img src={item.photo.mobile.s} alt={item.name} />
+                        {/* [複数要素].写真.携帯向け//[複数要素].掲載店名 */}
+                      </div>
+                    </div>
+                    <div>
+                      <div>{item.name}</div>
+                      {/* [複数要素].掲載店名 */}
+                      <div>
+                        <div>
+                          <span>{item.genre.name}</span>
+                          {/* [複数要素].お店ジャンル.お店ジャンル名 */}
+                          <span>{item.catch}</span>
+                          {/* [複数要素].お店キャッチ */}
+                        </div>
+                        <p>{item.access}</p>
+                        {/* [複数要素].交通アクセス */}
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
